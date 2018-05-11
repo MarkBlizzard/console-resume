d2b7008d41466af2d1dc605b8325142ccacca1b8
MarkBlizzard <mjblizzard93@gmail.com>
2018-05-11 11:32:02 -0500

    Add name, job, skills, interests, field, and aboutMe to JS

---
+++ app.js
@@ -0,0 +1,32 @@
+//Variables
+
+var name = 'Mark Blizzard';
+var upper = name.toUpperCase()
+    console.log(upper)
+
+var field = 'Student at IABHam';
+    console.log(field)
+
+var aboutMe = 'I am a 24 year old male whom recently took interest into coding';
+    console.log(aboutMe)
+
+//Arrays    
+var interests = ['Gaming', 'Coding', 'Anime', 'Music', 'Movies', 'Food'];
+    console.log(interests)
+
+var comp = ['Publix', 'Gamestop', 'Merck', 'AVAC', 'King Nut'];
+    console.log(comp)
+
+var title = ['Front Service Clerk', 'Game Advisor', 'Assembly Worker', 'Kennel Tech', 'Machine Operator'];
+    console.log(title)
+
+var desc = ["Assist customers, Bag groceries, Clean, Gather carts", 
+        "Recommend and Sell games to customers, Stock games alphabetically, Operate register, Clean",
+        "Pack product, Maintain quality, Stack boxes for shipping",
+        "Feed and Water animals, Walk and Play with animals, Clean after animals and Bathe them, Setup for boarding and retrieve animals for customers",
+        "Operate Machine, Pack product, Stack product for shipping, Maintain paperwork for Quality Assurance, Assemble and Dissasemble for thourough cleaning and product change"];
+    console.log(desc)
+
+var skills = ['Piano', 'BAM: HTML', 'BAM: CSS', 'BAM: JavaScript', 'Gaming'];
+    console.log(skills)
+
